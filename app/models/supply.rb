e567246265f2f0ddb78986f7edd25f72671eefbd
Feifan Zhou <feifan@me.com>
2014-05-10 17:59:50 -0400

    Case statement not working

--- app/models/supply.rb
+++ app/models/supply.rb
@@ -15,14 +15,13 @@ class Supply < ActiveRecord::Base
   belongs_to :appointment
 
   def name
-    case self.supply_id
-      when 1 return 'Tape dispenser'
-      when 2 return 'Bubble wrap'
-      when 7 return 'Packing tape'
-      when 26 return 'Small box'
-      when 27 return 'Large box'
-      when 35 return 'TV box'
-      else return 'Unknown'
+    if self.supply_id == 1 return 'Tape dispenser'
+    elsif self.supply_id == 2 return 'Bubble wrap'
+    elsif self.supply_id == 7 return 'Packing tape'
+    elsif self.supply_id == 26 return 'Small box'
+    elsif self.supply_id == 27 return 'Large box'
+    elsif self.supply_id == 35 return 'TV box'
+    else return 'Unknown'
     end
   end
 end
